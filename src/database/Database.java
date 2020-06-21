ae8f28e010186af80436e4dd9c7e078b34446e21
Paul Kunda <paul@localhost.localdomain>
2020-06-21 16:50:36 +0200

    SQL Changes and Connections

--- src/database/Database.java
+++ src/database/Database.java
@@ -55,7 +55,7 @@ public class Database {
     public static Connection getCon() {
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            con = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/finalyearproject", "root", "");
+            con = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/theEvansFinger", "root", "");
             return con;
         } catch (SQLException e) {
             return null;
